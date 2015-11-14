f9d25b6f0fee29fcef28ee1ab7364d1a7b2af18c
Nao Minami <south37777@gmail.com>
2015-11-14 18:58:21 +0900

    first commit

---
+++ cookbooks/development/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: development
+# Recipe:: default
+#
+# Copyright 2015, south37
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'haskell-platform' do
+  action :install
+end
+
